0bca2eaed7042feccf741c704d4c501531bafd42
Daniel <DanielEmig@gmx.de>
2022-05-06 05:34:49 +0200

    Add shader experiments

--- Assets/Scripts/Pixelation/PixelationShaderHandler.cs
+++ Assets/Scripts/Pixelation/PixelationShaderHandler.cs
@@ -7,8 +7,21 @@ public class PixelationShaderHandler : MonoBehaviour
 {
     public Material effectMaterial;
 
+    private Vector2Int lastResolution;
+
+    void OnPreRender()
+    {
+        if (lastResolution.x != Screen.width || lastResolution.y != Screen.height)
+        {
+            lastResolution = new Vector2Int(Screen.width, Screen.height);
+            effectMaterial.SetFloat("_pixelsX", Screen.width);
+            effectMaterial.SetFloat("_pixelsY", Screen.height);
+        }
+    }
+
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        Camera.main.depthTextureMode = DepthTextureMode.DepthNormals;
         if (enabled && effectMaterial != null)
         {
             Graphics.Blit(source, destination, effectMaterial);
